7ba2f9c3101e71b2f63c68dccdca502b85e09ff1
Raphael Portailler <raphael.portailler@lapiscine.pro>
2024-07-12 11:49:08 +0200

    Modif Affichage de commande.php

--- views/commande.php
+++ views/commande.php
@@ -5,15 +5,15 @@
         <!-- on effectue une boucle pour parcourir le tableau des commandes stockés dans la variables $orders et venir créer un article de chacune de ces commandes -->
             <?php foreach($orders as $order){ ?>
                 <article>
-                    <p>ID :<?php echo $order['id']; ?></p>
-                    <p>Customer :<?php echo $order['customer']; ?></p>
-                    <p>Amout :<?php echo $order['amount']; ?></p>
+                    <p>ID :<?php echo " " . $order['id']; ?></p>
+                    <p>Customer :<?php echo " " . $order['customer']; ?></p>
+                    <p>Amout :<?php echo " " . $order['amount']; ?></p>
                     <?php foreach($order['products'] as $product){ ?>
-                            <p>Produit :<?php echo $product; ?><p>
+                            <p>Produit :<?php echo " " . $product; ?><p>
                     <?php } ?>
                     <!-- On viens transformer la date enregistrée dans un nouveau format pour l'affichage -->
                     <?php $date = new DateTime($order['date']); ?>
-                    <p>Date :<?php echo $date->format("d/m/Y")?></p>
+                    <p>Date :<?php echo " " . $date->format("d/m/Y")?></p>
                     <br>
                 </article>
             <?php } ?>
