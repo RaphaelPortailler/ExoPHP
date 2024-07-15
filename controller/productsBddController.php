576ccc8b4a271e50a35a55974e3e76470b5b7463
Raphael Portailler <raphael.portailler@lapiscine.pro>
2024-07-15 15:27:10 +0200

    execution requete SQL

---
+++ controller/productsBddController.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('../config/config.php');
+
+// On stock dans une variable la requete SQL que l'on prepare pour l'executer
+$sql = "INSERT INTO product (titre, sous_titre, descriptions) VALUES (:titre, :sous_titre, :descriptions)";
+$stmt = $pdo->prepare($sql);
+
+// Définir les paramètres et exécuter
+$titre = "Exemple de titre";
+$sous_titre = "Exemple de sous-titre";
+$description = "Ceci est une description d'exemple pour le produit.";
+
+// Avec bindParam on viens vérifier que la valeur de chaque élément a inserer dans notre colonne article ne soit pas une VRAI requete SQL 
+$stmt->bindParam(':titre', $titre);
+$stmt->bindParam(':sous_titre', $sous_titre);
+$stmt->bindParam(':description', $description);
+
+// Exécuter la requête stocker dans sa variable 
+if ($stmt->execute()) {
+	echo "Nouveau produit ajouté avec succès";
+} else {
+	echo "Erreur lors de l'ajout du produit";
+}
